b1df04ccab18e502135563567291067509e8b0e0
Chung Anh <chunganhbk@gmail.com>
2018-09-11 20:02:37 +0700

    implement identity mongodb asp core 2.1 mongodb driver 2.7

---
+++ IndexChecks.cs
@@ -0,0 +1,62 @@
+﻿namespace Microsoft.AspNetCore.Identity.MongoDB
+{
+	using global::MongoDB.Driver;
+
+	public static class IndexChecks
+	{
+        public static void EnsureUniqueIndexOnNormalizedUserName<TUser>(IMongoCollection<TUser> users)
+            where TUser : IdentityUser
+        {
+            var userBuilder = Builders<TUser>.IndexKeys;
+            var uniqueUserName = new CreateIndexModel<TUser>(userBuilder.Ascending(t => t.NormalizedUserName), new CreateIndexOptions { Unique = true });
+            users.Indexes.CreateOneAsync(uniqueUserName);
+		}
+
+		public static void EnsureUniqueIndexOnNormalizedRoleName<TRole>(IMongoCollection<TRole> roles)
+			where TRole : IdentityRole
+		{
+            var roleBuilder = Builders<TRole>.IndexKeys;
+            var uniqueRoleName = new CreateIndexModel<TRole>(roleBuilder.Ascending(t => t.NormalizedName), new CreateIndexOptions { Unique = true });
+			roles.Indexes.CreateOneAsync(uniqueRoleName);
+		}
+
+		public static void EnsureUniqueIndexOnNormalizedEmail<TUser>(IMongoCollection<TUser> users)
+			where TUser : IdentityUser
+		{
+            var userBuilder = Builders<TUser>.IndexKeys;
+            var uniqueEmail = new CreateIndexModel<TUser>(userBuilder.Ascending(t => t.NormalizedEmail), new CreateIndexOptions { Unique = true });
+			users.Indexes.CreateOneAsync(uniqueEmail);
+		}
+
+		/// <summary>
+		///     ASP.NET Core Identity now searches on normalized fields so these indexes are no longer required, replace with
+		///     normalized checks.
+		/// </summary>
+		public static class OptionalIndexChecks
+		{
+			public static void EnsureUniqueIndexOnUserName<TUser>(IMongoCollection<TUser> users)
+				where TUser : IdentityUser
+			{
+                var userBuilder = Builders<TUser>.IndexKeys;
+                var uniqueUserName = new CreateIndexModel<TUser>(userBuilder.Ascending(t => t.UserName), new CreateIndexOptions { Unique = true });
+                users.Indexes.CreateOneAsync(uniqueUserName);
+			}
+
+			public static void EnsureUniqueIndexOnRoleName<TRole>(IMongoCollection<TRole> roles)
+				where TRole : IdentityRole
+			{
+                var roleBuilder = Builders<TRole>.IndexKeys;
+                var uniqueRoleName = new CreateIndexModel<TRole>(roleBuilder.Ascending(t => t.Name), new CreateIndexOptions { Unique = true });
+                roles.Indexes.CreateOneAsync(uniqueRoleName);
+			}
+
+			public static void EnsureUniqueIndexOnEmail<TUser>(IMongoCollection<TUser> users)
+				where TUser : IdentityUser
+            {
+                var userBuilder = Builders<TUser>.IndexKeys;
+                var uniqueEmail = new CreateIndexModel<TUser>(userBuilder.Ascending(t => t.Email), new CreateIndexOptions { Unique = true });
+                users.Indexes.CreateOneAsync(uniqueEmail);
+            }
+		}
+	}
+}
---
+++ MongoIdentityBuilderExtensions.cs
@@ -0,0 +1,87 @@
+﻿// ReSharper disable once CheckNamespace - Common convention to locate extensions in Microsoft namespaces for simplifying autocompletion as a consumer.
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	using System;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.MongoDB;
+    using MongoDB.Driver;
+
+	public static class MongoIdentityBuilderExtensions
+	{
+		/// <summary>
+		///     This method only registers mongo stores, you also need to call AddIdentity.
+		///     Consider using AddIdentityWithMongoStores.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <param name="connectionString">Must contain the database name</param>
+		public static IdentityBuilder RegisterMongoStores<TUser, TRole>(this IdentityBuilder builder, IMongoDatabase database)
+			where TRole : IdentityRole
+			where TUser : IdentityUser
+		{
+			return builder.RegisterMongoStores(
+				p => database.GetCollection<TUser>("ApplicationUser"),
+				p => database.GetCollection<TRole>("ApplicationRole"));
+		}
+
+		/// <summary>
+		///     If you want control over creating the users and roles collections, use this overload.
+		///     This method only registers mongo stores, you also need to call AddIdentity.
+		/// </summary>
+		/// <typeparam name="TUser"></typeparam>
+		/// <typeparam name="TRole"></typeparam>
+		/// <param name="builder"></param>
+		/// <param name="usersCollectionFactory"></param>
+		/// <param name="rolesCollectionFactory"></param>
+		public static IdentityBuilder RegisterMongoStores<TUser, TRole>(this IdentityBuilder builder,
+			Func<IServiceProvider, IMongoCollection<TUser>> usersCollectionFactory,
+			Func<IServiceProvider, IMongoCollection<TRole>> rolesCollectionFactory)
+			where TRole : IdentityRole
+			where TUser : IdentityUser
+		{
+			if (typeof(TUser) != builder.UserType)
+			{
+				var message = "User type passed to RegisterMongoStores must match user type passed to AddIdentity. "
+				              + $"You passed {builder.UserType} to AddIdentity and {typeof(TUser)} to RegisterMongoStores, "
+				              + "these do not match.";
+				throw new ArgumentException(message);
+			}
+			if (typeof(TRole) != builder.RoleType)
+			{
+				var message = "Role type passed to RegisterMongoStores must match role type passed to AddIdentity. "
+				              + $"You passed {builder.RoleType} to AddIdentity and {typeof(TRole)} to RegisterMongoStores, "
+				              + "these do not match.";
+				throw new ArgumentException(message);
+			}
+			builder.Services.AddSingleton<IUserStore<TUser>>(p => new UserStore<TUser, TRole>(usersCollectionFactory(p), rolesCollectionFactory(p)));
+			builder.Services.AddSingleton<IRoleStore<TRole>>(p => new RoleStore<TRole>(rolesCollectionFactory(p)));
+			return builder;
+		}
+
+        /// <summary>
+        ///     This method registers identity services and MongoDB stores using the IdentityUser and IdentityRole types.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="database">mongodb database connext</param>
+        public static IdentityBuilder AddIdentityWithMongoStores(this IServiceCollection services, IMongoDatabase database)
+		{
+			return services.AddIdentityWithMongoStoresUsingCustomTypes<IdentityUser, IdentityRole>(database);
+		}
+
+		/// <summary>
+		///     This method allows you to customize the user and role type when registering identity services
+		///     and MongoDB stores.
+		/// </summary>
+		/// <typeparam name="TUser"></typeparam>
+		/// <typeparam name="TRole"></typeparam>
+		/// <param name="services"></param>
+		/// <param name="connectionString">Connection string must contain the database name</param>
+		public static IdentityBuilder AddIdentityWithMongoStoresUsingCustomTypes<TUser, TRole>(this IServiceCollection services, IMongoDatabase database)
+			where TUser : IdentityUser
+			where TRole : IdentityRole
+		{
+			return services.AddIdentity<TUser, TRole>()
+				.RegisterMongoStores<TUser, TRole>(database);
+		}
+	}
+}
